de90f4547afd434790580f32124f5209ddd7d948
ThuHuynh2809 <124646995+ThuHuynh2809@users.noreply.github.com>
2023-10-01 16:55:01 +0700

    ThuHUynh2809
    up firs, 1_a, 1_b

---
+++ FamiPoint/App.js
@@ -0,0 +1,8 @@
+import FirstScreen from './screen/FirstScreen';
+import oneA from './screen/1_a';
+import oneB from './screen/1_b';
+
+
+//export default oneA;
+//export default oneB;
+export default FirstScreen;
